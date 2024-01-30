0386edc2d04550845f7fd0e73e58b6a280d8c209
bkmnst <bakemonsuta@gmail.com>
2024-01-30 21:54:28 +0200

    add ability to create different field sizes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const DEFAULT_GRID_SIZE = 16;
 const container = document.querySelector("#container");
 
 const pixel = document.createElement("div");
@@ -5,12 +6,22 @@ pixel.classList.add("pixel");
 
 
 // Fill the grid field
-let gridSize = 16;
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const clonedPixel = pixel.cloneNode(true);
-    clonedPixel.addEventListener("mouseenter", (event) => {
-        event.target.classList.add("colored");
-    });
-    container.appendChild(clonedPixel);
+
+function createGrid(gridSize) {
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const clonedPixel = pixel.cloneNode(true);
+        
+        const percentageSize = 100 / gridSize;
+        clonedPixel.style.width = `${percentageSize}%`
+        clonedPixel.style.height = `${percentageSize}%`
+
+        clonedPixel.addEventListener("mouseenter", (event) => {
+            event.target.classList.add("colored");
+        });
+
+        container.appendChild(clonedPixel);
+    }
 }
 
+createGrid(36);
