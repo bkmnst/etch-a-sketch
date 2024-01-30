0a531c2b786f139c6d82f56ae91f648e27076201
bkmnst <bakemonsuta@gmail.com>
2024-01-30 15:28:47 +0200

    rename const to make more sense

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 const container = document.querySelector("#container");
 
-const squareDiv = document.createElement("div");
-squareDiv.className = "pixel";
+const pixel = document.createElement("div");
+pixel.className = "pixel";
 
 let gridSize = 16;
 for (let i = 0; i < gridSize; i++) {
-    container.appendChild(squareDiv.cloneNode(true));
+    container.appendChild(pixel.cloneNode(true));
 }
